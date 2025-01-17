ec1e9d75b0b63185fb23ec582f116314c4a34aab
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-01-17 09:53:12 +0800

    Add own solution to 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targets) {
+    for(const target of targets){
+        for(let i = 0; i < array.length; i++){
+            if(array[i] === target){
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
