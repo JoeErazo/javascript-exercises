37030ec6d8af60e16c8e1634bc7484c2c8fc620b
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-01-17 10:06:27 +0800

    Add own solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(num1 !== Math.floor(num1) || num2 !== Math.floor(num2) ||
+        num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    let sum = 0;
+    let min, max;
+    if(num1 < num2){
+        min = num1;
+        max = num2;
+    }
+    else {
+        min = num2;
+        max = num1;
+    }
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
