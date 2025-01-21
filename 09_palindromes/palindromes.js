04c383f959d98bfc09eb9ab6c7eca477b6a6fe36
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-01-21 10:49:32 +0800

    Revise own solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,22 +1,11 @@
 const palindromes = function (str) {
-    const alphaNumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-        'j', 'k', 'l', 'm', 'n', 'p', 'p', 'q', 'r', 's', 't', 'u',
-        'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-        'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5',
-        '6', '7', '8', '9', '0'];
-    let temp = "";
-    for(let char of str) {
-        if(alphaNumeric.includes(char)) temp += char.toLowerCase();
-    }
-    let strReversed = "";
-    for(let char of str) {
-        strReversed = char + strReversed;
-    }
-    let tempReversed = "";
-    for(let char of strReversed) {
-        if(alphaNumeric.includes(char)) tempReversed += char.toLowerCase();
-    }
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const temp = str
+                .toLowerCase()
+                .split("")
+                .filter((char) => alphanumeric.includes(char))
+                .join("");
+    const tempReversed = temp.split("").reverse().join("");
     return temp === tempReversed;
 };
 
