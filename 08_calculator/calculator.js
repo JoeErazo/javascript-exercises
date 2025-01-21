17ec14022fa139b2c66d040bf07b8ddc80a4d40e
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-01-21 10:19:41 +0800

    Add own solution to 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length > 0)
+	  return arr.reduce((sum, num) => sum + num);
+  else
+    return 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length > 0)
+	  return arr.reduce((product, num) => product * num);
+  else
+    return 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0) return 1;
+	let temp = [];
+  for(let i = 1; i <= n; i++){
+    temp.push(i);
+  }
+  return multiply(temp);
 };
 
 // Do not edit below this line
