38c8404254b925266be19d19002ffdad17e0b5ac
Johann Erazo <89018154+JoeErazo@users.noreply.github.com>
2025-01-21 11:12:35 +0800

    Add own solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    else if(n == 0) return 0;
+    else{
+        let lastTwo = [0, 1];
+        for(let i = 1; i < n; i++){
+            let temp = lastTwo[0] + lastTwo[1];
+            lastTwo.shift();
+            lastTwo.push(temp);
+        }
+        return lastTwo[1];
+    }
 };
 
 // Do not edit below this line
